83fee4dfc2f65bbebbef94d5ad85a1d1e061591a
Milinda Hashan Kumara <165372276+Milindahashankumara@users.noreply.github.com>
2024-06-22 21:14:37 +0530

    coding01

--- index.py
+++ index.py
@@ -1,15 +1,36 @@
-# Get input from the user
-a = input("Enter value a = ");
-b = input("Enter value b = ");
+"""name = "Milinda Hashan"
+print(name[8])
+"""
+incorrect
 
-# Convert inputs to integers
-a = int(a)
-b = int(b)
+length=len("jenny mili")
+print("Your name has" + lenght + "character")
 
-# Swap values
-a = a + b
-b = a - b
-a = a - b
+correct
 
-# Print the swapped values
-print("a =", a, '\n', "b =", b);
+length=len("jenny mili")
+
+print("Your name has " + str (length) + " character")
+print(type(length))
+new_length= str(length)
+print(type(new_length))
+
+
+
+
+01)
+first_number=input("Enter 01 Number:")
+second_number=input("Enter 02 NUmber")
+
+sum = first_number + second_number
+
+print(sum)
+
+02)
+first_number=input("Enter 01 Number:")
+second_number=input("Enter 02 NUmber")
+
+sum = int(first_number) +int (second_number)
+
+print(sum)
+
